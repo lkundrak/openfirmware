lkundrak/openfirmware

commit 2d7dad95a1f4becbbc4cb0fdf7a658e36180d3b0

arm/linux: ignore the size from the zImage header
We already got the file size and that one is good enough.

Actually, it's one better: with CONFIG_ARM_APPENDED_DTB you can slap on
the DTB to the end of the image file, and still have the kernel see it.
If we were only considering the zImage size, we'd be blind to that.

cpu/arm/linux.fth
@@ -166,14 +166,12 @@ defer place-ramdisk
 
 : init-zimage?   ( -- flag )
    loaded                               ( adr len )
-   h# 30 <  if  drop false exit  then   ( adr )
-   dup h# 24 + l@  h# 016f2818  <>  if  drop false exit  then   ( adr )
-   >r
-   r@ h# 28 + l@  r@ +                  ( start r: adr )
-   r@ h# 2c + l@  r@ +                  ( start end r: adr )
-   r> drop                              ( start end )
-   over -                               ( start len )
-   
+   dup h# 30 <  if  drop false exit  then   ( adr len )
+   over h# 24 + l@  h# 016f2818  <>  if  drop false exit  then   ( adr len )
+   swap >r                              ( len r: adr )
+   r@ h# 28 + l@  r@ +                  ( len start r: adr )
+   swap r> drop                         ( start len )
+
    linux-base swap move                 ( )
 
    true to linux-loaded?                ( )

commit ebcafa1ba7e97ec588f17f6df4c2ebc656769871

arm/linux: drop load-base off stack if not a zImage
At the time we check the zImage magic, there's ( adr len ) on the stack.
Pop both off if the magic check fails.

Otherwise the load of a different kind of image (such as Forth text larger
than 30 bytes -- we check the length on the previous line) would leave the
load base on stack which is just not nice.

Fixes: 2d7dad95a1f4 ("arm/linux: ignore the size from the zImage header")

cpu/arm/linux.fth
@@ -167,7 +167,7 @@ defer place-ramdisk
 : init-zimage?   ( -- flag )
    loaded                               ( adr len )
    dup h# 30 <  if  2drop false exit  then   ( adr len )
-   over h# 24 + l@  h# 016f2818  <>  if  drop false exit  then   ( adr len )
+   over h# 24 + l@  h# 016f2818  <>  if  2drop false exit  then   ( adr len )
    swap >r                              ( len r: adr )
    r@ h# 28 + l@  r@ +                  ( len start r: adr )
    swap r> drop                         ( start len )