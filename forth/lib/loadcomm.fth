lkundrak/openfirmware

commit 95a5761e0db0a4f525bca0a8d752cdaab4c31a54

Add support for arm64.

cpu/arm64/tools.bth
@@ -1,0 +1,78 @@
+\ See license at end of file
+purpose: Load file for Forth toolkit, without firmware
+
+dictionary: ${BP}/cpu/arm64/build/kernel.dic
+command: &arm64forth &dictionary &this
+build-now
+
+\ ' $report-name is include-hook
+\ ' $report-name+ is include-hook  \ Get source file name + stack dump
+\ ' noop is include-hook
+
+\ who ate my memory?
+variable oldsize
+: rn   ( a n -- )
+   here origin - oldsize !
+   (cr 2dup type 64 over - spaces
+;
+\ ' rn is include-hook
+: showsize   ( -- )
+   base @ >r hex
+   here origin - dup . oldsize @ - . cr
+   r> base !
+;
+\ ' showsize is include-exit-hook
+
+" tools.tag" r/w create-file drop  tag-file !
+
+create resident-packages
+
+fload ${BP}/forth/lib/fwsetup.fth
+
+transient
+true value assembler?                    \ False to discard assembler after compilation
+resident
+
+fload ${BP}/forth/lib/loadcomm.fth       \ CPU-independent Forth tools
+fload ${BP}/cpu/arm64/utils.fth          \ 
+fload ${BP}/cpu/arm64/loadmach.fth       \ CPU and OS-specific extensions
+
+: for     ( -- >m <m )     ['] (for)   +>mark     +<mark	; immediate
+: next    ( >m <m -- )     ['] (next)  -<resolve  ->resolve	; immediate
+
+fload ${BP}/cpu/arm64/enhanced.fth
+
+fload ${BP}/cpu/arm64/call-forth.fth
+
+[ifndef] no-heads
+resident   dispose   0 0 set-transize    \ true is suppress-transient
+
+close-tagfile
+
+h# 10.0000  dictionary-size !
+.( --- Saving tools.dic --- )  " tools.dic" $save-forth cr
+[then]
+
+\ LICENSE_BEGIN
+\ Copyright (c) 1994 FirmWorks
+\
+\ Permission is hereby granted, free of charge, to any person obtaining
+\ a copy of this software and associated documentation files (the
+\ "Software"), to deal in the Software without restriction, including
+\ without limitation the rights to use, copy, modify, merge, publish,
+\ distribute, sublicense, and/or sell copies of the Software, and to
+\ permit persons to whom the Software is furnished to do so, subject to
+\ the following conditions:
+\
+\ The above copyright notice and this permission notice shall be
+\ included in all copies or substantial portions of the Software.
+\
+\ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+\ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+\ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+\ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+\ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+\ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+\ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+\
+\ LICENSE_END

forth/lib/loadcomm.fth
@@ -73,6 +73,7 @@ fload ${BP}/forth/lib/lex.fth
 fload ${BP}/forth/lib/printf.fth
 
 fload ${BP}/forth/lib/autold.fth		\ Autoload mechanism
+fload ${BP}/forth/lib/1x0.fth
 
 [ifndef] partial-no-heads	transient  [then]
 fload ${BP}/forth/lib/initsave.fth		\ Common code for save-forth et al

forth/lib/1x0.fth
@@ -1,0 +1,92 @@
+\ Imprecise
+\ XXX note: move this code out of the disassembler, and add arm32 and x64 support
+
+\ turn imprecise spec line into value and mask
+\ char mask value
+\ 1    1    1
+\ 0    1    0
+\ x    0    0
+\ _    ignored
+
+\ example
+\ " 010xxx01" 1x0    gives ( 11100011 01000001 )
+: 1x0   ( $ -- mask value )
+   >r  0 0 rot    ( m v a )
+   begin  r@  while
+      dup c@ case        ( m v a )
+	 ascii 1 of
+	    rot 2* 1 or 
+	    rot 2* 1 or
+	    rot
+	 endof
+	 ascii 0 of
+	    rot 2* 1 or 
+	    rot 2*
+	    rot	    
+	 endof
+	 ascii x of
+	    rot 2* 
+	    rot 2* 
+	    rot
+	 endof
+	 ascii _ of	 endof
+	 true abort" invalid 1x0 string"
+      endcase
+      1+
+      r> 1- >r   \ count
+   repeat    ( m v a )
+   r> 2drop
+;
+
+\ [ifndef] (1x0)
+\ \ push a pair of 32-bit in-line values 
+\ code (1x0)   ( -- mask value )
+\    push    tos,sp
+\    ldr     w0,[ip],#4
+\    push    x0,sp
+\    ldr     wtos,[ip],#4
+\ c;
+\ [then]
+
+\ convert string and compile a pair of 32-bit values
+: 1x0"   ( -- )   ascii " parse 1x0 postpone (1x0)  swap l, l,  ; immediate
+
+\ convert string and compile a pair of 64-bit values
+\ : x1x0"   ( -- )   ascii " parse 1x0 postpone (x1x0)  swap , ,  ; immediate
+
+\ [ifndef] (af)
+\ \ "and of"
+\ \ essentially   " selector mask and value = if drop  "
+\ code (af)  ( selector mask val -- [ selector ] )
+\    mov     x0,tos     \ val
+\    pop     x1,sp      \ mask
+\    pop     tos,sp     \ selector
+\    and     x1,x1,tos
+\    cmp     x1,x0
+\    <> if
+\       ldrsw x0,[ip]
+\       add   ip,ip,x0
+\       next
+\    then
+\    pop     tos,sp
+\    inc     ip,#/token
+\ c;
+\ [then]
+
+: af     ( -- >m )  ['] (af)  +>mark  ; immediate
+alias endaf endof immediate
+
+
+also hidden
+\ Decompiler extensions for 1x0 and af support
+\ note: 1x0 decompiles as a pair of literals
+\ maybe later show the source string instead
+: .1x0       ( ip -- ip' )  ta1+ dup l@ . la1+  dup l@ . la1+  ; 
+: skip-1x0   ( ip -- ip' )  ta1+ la1+ la1+  ;
+: .(1x0)     ( ip -- ip' )  .." af   " +branch  ;
+' (1x0)  ' .1x0  ' skip-1x0  install-decomp
+      
+: .af        ( ip -- ip' )  .." af   " +branch  ;
+' (af)  ' .af  ' scan-of  install-decomp
+previous
+

commit 5f284c8c5777c83b12a94fefbc2fd65f3c08b4a9

lib: move 1x0.fth into cpu/arm64/
It relies on native (1x0) word that's only implemented or used on arm64.
Otherwise all other platforms are broken:

  ${BP}/forth/lib/1x0.fth:52: (1x0) ?
  make: *** [emuofw.rom] Error 1

Fixes: 95a5761e0 ('Add support for arm64.')

cpu/arm64/tools.bth
@@ -34,6 +34,7 @@ true value assembler?                    \ False to discard assembler after comp
 resident
 
 fload ${BP}/forth/lib/loadcomm.fth       \ CPU-independent Forth tools
+fload ${BP}/cpu/arm64/1x0.fth
 fload ${BP}/cpu/arm64/utils.fth          \ 
 fload ${BP}/cpu/arm64/loadmach.fth       \ CPU and OS-specific extensions
 

forth/lib/loadcomm.fth
@@ -73,7 +73,6 @@ fload ${BP}/forth/lib/lex.fth
 fload ${BP}/forth/lib/printf.fth
 
 fload ${BP}/forth/lib/autold.fth		\ Autoload mechanism
-fload ${BP}/forth/lib/1x0.fth
 
 [ifndef] partial-no-heads	transient  [then]
 fload ${BP}/forth/lib/initsave.fth		\ Common code for save-forth et al